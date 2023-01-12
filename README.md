2b05031c9a6b5afc4653cb72201e1549bf0fa830
Maksura Mahjabeen <maksura.cse@gmail.com>
2023-01-12 20:21:54 +0100

    spring MVC demo project

---
+++ README.md
@@ -0,0 +1,2 @@
+<!-- Demo tw0 -->
+This is a smple project for learning spring MVC
---
+++ src/com/selfcode/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,76 @@
+package com.selfcode.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+   
+	//need a controller to show the initial HTML  form
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	//need a controller method to process the HTML form
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	//a new controller method to read form data
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		//read the request param from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		//convert the data to all caps
+		theName = theName.toUpperCase();
+		//create the message
+		String result = "Yo! " + theName;
+		
+		//add message to the model 
+		
+		model.addAttribute("message",result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	//a new controller method to read form data
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName")String theName, Model model) {
+		
+		
+		
+		
+		//convert the data to all caps
+		theName = theName.toUpperCase();
+		//create the message
+		String result = "Hi! " + theName  + "  from V3";
+		
+		//add message to the model 
+		
+		model.addAttribute("message",result);
+		
+		return "helloworld";
+	}
+	
+	
+	
+	
+	
+}
